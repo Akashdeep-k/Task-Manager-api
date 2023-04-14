f535b95585a431fe96f774c296b81d2701228986
Akash <iamakashkataria18@gmail.com>
2023-04-15 02:45:04 +0530

    added setup for routes

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,7 +1,29 @@
-const getAllTasks = (req, res)=>{
-    res.send("all tasks")
+const getAllTasks = (req, res) => {
+    res.send("get all tasks")
+}
+
+const getTask = (req, res) => {
+    const {id} = req.params
+    res.json({"id": id})
+}
+
+const createTask = (req, res) => {
+    res.json(req.body)
+}
+
+const updateTask = (req, res) => {
+    const {id} = req.params
+    res.json({"id": id})
+}
+const deleteTask = (req, res) => {
+    const {id} = req.params
+    res.json({"id": id})
 }
 
 module.exports = {
-    getAllTasks
+    getAllTasks,
+    getTask,
+    createTask,
+    updateTask,
+    deleteTask
 }
